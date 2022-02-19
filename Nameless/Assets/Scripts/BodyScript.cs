568e78d28d73fd710d8c69794a52eb06f53aed5a
AHMET KAAN AVCI <kaan.cred@gmail.com>
2022-02-20 02:36:05 +0300

    Death effect added

---
+++ Nameless/Assets/Scripts/BodyScript.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BodyScript : MonoBehaviour
+{
+   public bool life= false;
+   
+    
+    void Start()
+    {
+        Death();
+    }
+
+    
+    void Update()
+    {
+
+        
+
+    }
+
+    public void Death()
+    {
+
+
+        StartCoroutine(waiter());
+        
+
+    }
+
+    IEnumerator waiter()
+    {
+
+
+        for (int i = 0; i < 5; i++)
+        {
+
+
+
+            GetComponent<SpriteRenderer>().material.color = Color.red;
+            yield return new WaitForSeconds(0.1f);
+            GetComponent<SpriteRenderer>().material.color = Color.white;
+            yield return new WaitForSeconds(0.1f);
+
+
+
+
+        }
+        
+    }
+
+}
