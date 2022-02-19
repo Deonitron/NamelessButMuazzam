35bf13ac8c8ac63a43d1bddbd6329c4182d4afc2
AHMET KAAN AVCI <kaan.cred@gmail.com>
2022-02-19 14:00:56 +0300

    background added

--- Nameless/Assets/Scripts/GameManager.cs
+++ Nameless/Assets/Scripts/GameManager.cs
@@ -4,13 +4,20 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    float x1;
+    float y1;
+    Vector2 Vector31;
     void Start()
     {
-         
+        Vector31 = new Vector2(x1, y1);
+
+            
     }
 
     void Update()
     {
-        
+        Camera.main.transform.position = Vector31;
+
+        x1 += 0.01f;
     }
 }
