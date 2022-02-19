73366e502a02a6ec1af101a5835ca76c52f4216a
Ahmet Çelik <ahmet.celik2605@outlook.com>
2022-02-19 14:29:12 +0300

    Camera movement added

--- Nameless/Assets/Scripts/GameManager.cs
+++ Nameless/Assets/Scripts/GameManager.cs
@@ -5,17 +5,17 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     float x1;
-    float y1;
     Vector2 Vector31;
     void Start()
     {
-        Vector31 = new Vector2(x1, y1);
 
             
     }
 
     void Update()
     {
+        Vector31 = new Vector2(x1, Camera.main.transform.position.y);
+
         Camera.main.transform.position = Vector31;
 
         x1 += 0.01f;
